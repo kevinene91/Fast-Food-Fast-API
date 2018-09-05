ff10635b4db9d2cde64ab24d8bb562e5c96d4c12
kevinene91 <kevinene91@gmail.com>
2018-09-05 17:07:36 +0300

    [Chore #160287156] pytest

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 orders =[
-    {
+    {    "id": 1,   
          "food": "chips",
          "quantity": 2, 
          "price": 200,
          "status": "pending"   
     },
-    {
+    {   "id": 2,
         "food": "kuku",
         "quantity": 3, 
         "price": 1500,
