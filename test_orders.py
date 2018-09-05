ddc70a800d761ea6db0353ea0eed5efb4b2443af
kevinene91 <kevinene91@gmail.com>
2018-09-05 05:21:46 +0300

    [Bug #160274480] fix response codes

--- test_orders.py
+++ test_orders.py
@@ -9,6 +9,7 @@ class OrdersTestCase(unittest.TestCase):
         self.app = create_app(config_name="testing")
         self.client = self.app.test_client()
         self.order = {
+            "id": 1,
             "food": "chips",
             "quantity": 2, 
             "price": 200,
@@ -20,28 +21,27 @@ class OrdersTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_resource_order_add(self):
-        response = self.client.post('api/v1/order', data=json.dumps(self.order))
-        self.assertEqual(response.status_code, 201)
+        response = self.client.post('api/v1/orders', data=self.order)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('chips', str(response.data))
+        
 
     def test_resource_order_get_by_id(self): 
-        response = self.client.get('api/v1/order/1')
+        response = self.client.get('api/v1/orders/1')
         self.assertEqual(response.status_code, 200)
 
     def test_resource_order_edit(self): 
-        # get_order_to_edit = self.client.get('api/v1/order/1')
-        response = self.client.put('api/v1/order/1', data ={
+        response = self.client.put('api/v1/orders/1', data ={
+            "id": 5,
             "food": "chips",
             "quantity": 2, 
             "price": 200,
             "status": "completed"
         })
-        self.assertEqual(response.status_code, 201)
-        result = self.client.get('api/v1/order/1')
-        self.assertIn('complted', str(result.data))
+        self.assertEqual(response.status_code, 200)
+        result = self.client.get('api/v1/orders/1')
+        self.assertIn('completed', str(result.data))
         
 
 if __name__ == "__main__":
     unittest.main()
-
-
-
