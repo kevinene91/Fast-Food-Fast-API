b04891a862746ef058bda3a71408ef77ed6896f4
kevinene91 <kevinene91@gmail.com>
2018-09-06 22:15:53 +0300

    [Chore] improve message

--- app/resources/orders.py
+++ app/resources/orders.py
@@ -3,34 +3,27 @@ from flask_restful import Resource, reqparse
 from app.models import orders
 
 class OrderListResource(Resource):
-    def get(self): 
-        return {'orders':orders}
-
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("id",type=int, 
-                required=True,
-        )
-        parser.add_argument("food",type=str,
-                required=True,
-        )
-        parser.add_argument("quantity",type=int,
-                required=True,
-        )
-        parser.add_argument("price",type=int,
-                required=True,
-        )
-        parser.add_argument("status",type=str,
-                required=True,
-        )
-        data = parser.parse_args()
-        order = {
-            'id': data['id'], 'food':data['food'],
-            'quantity':data['quantity'],'price':data['price'],
-            'status':data['status'] 
-                }
-        orders.append(order)
-        return order, 201
+	def get(self): 
+		return {'orders':orders}
+	def post(self):
+		inc_id = len(orders) + 1
+		parser = reqparse.RequestParser()
+		parser.add_argument("food",type=str,
+		required=True)
+		parser.add_argument("quantity",type=int,
+		required=True)
+		parser.add_argument("price",type=int,
+		required=True)
+		parser.add_argument("status",type=str,
+		required=True)
+		data = parser.parse_args()
+		order = {
+		'id': inc_id, 'food':data['food'],
+		'quantity':data['quantity'],'price':data['price'],
+		'status':data['status'] 
+		}
+		orders.append(order)
+		return order, 201
 
 
 class OrderResource(Resource): 
@@ -50,6 +43,3 @@ class OrderResource(Resource):
 		else:
 			return {"message":"no item to update"}, 400
 		return order_to_edit, 201
-
-
-    
