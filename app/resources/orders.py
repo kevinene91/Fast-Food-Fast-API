a4f9c185e251ce9446ad403c586606a060b264ed
kevinene91 <kevinene91@gmail.com>
2018-09-06 21:16:55 +0300

    [Chore #160326839] add tests

--- app/resources/orders.py
+++ app/resources/orders.py
@@ -3,47 +3,32 @@ from flask_restful import Resource, reqparse
 from app.models import orders
 
 class OrderListResource(Resource):
-    
-    
-    def get(self): 
-        return {'orders':orders}
-
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument("id",
-                type=int, 
-                required=True,
-        )
-        parser.add_argument("food",
-                type=str,
-                required=True,
-        )
-        parser.add_argument("quantity",
-                type=int,
-                required=True,
-        )
-        parser.add_argument("price",
-                type=int,
-                required=True,
-        )
-        parser.add_argument("status",
-                type=str,
-                required=True,
-        )
-        data = parser.parse_args()
-        order = {
-            'id': data['id'], 'food':data['food'],
-            'quantity':data['quantity'],'price':data['price'],
-            'status':data['status'] 
+	def get(self): 
+		return {'orders':orders}
+	def post(self):
+                inc_id = len(orders) + 1
+                parser = reqparse.RequestParser()
+                parser.add_argument("food",type=str,
+                required=True)
+                parser.add_argument("quantity",type=int,
+                required=True)
+                parser.add_argument("price",type=int,
+                required=True)
+                parser.add_argument("status",type=str,
+                required=True)
+                data = parser.parse_args()
+                order = {
+                'id': inc_id, 'food':data['food'],
+                'quantity':data['quantity'],'price':data['price'],
+                'status':data['status'] 
                 }
-        orders.append(order)
-        return order
-
-        
+                orders.append(order)
+                return order, 201
 
 class OrderResource(Resource): 
     
     def get(self,id):
-        return {'order': next(filter(lambda x:x['id'] == id, orders), None)}
+        order = {'order': next(filter(lambda x:x['id'] == id, orders), None)}
+        return order
 
     
--- test_orders.py
+++ test_orders.py
@@ -4,6 +4,7 @@ from app import create_app
 
 app = create_app(config_name="testing")
 client = app.test_client()
+
 order = {
     "id": 1,
     "food": "chips",
@@ -12,31 +13,104 @@ order = {
     "status": "pending"
 }
 
+empty_order = {}
+
+status_less_order = {
+    "id": 1,
+    "food": "chips",
+    "quantity": 2, 
+    "price": 200,
+}
+
+price_less_order = {
+    "id": 1,
+    "food": "chips",
+    "quantity": 2, 
+    "status": "pending"  
+}
+quantity_less_order = {
+    "id": 1,
+    "food": "chips", 
+    "price": 200,
+    "status": "pending"
+}
+
+food_less_order = {
+    "id": 1, 
+    "quantity": 3,
+    "price": 200,
+    "status": "pending"
+}
+
+
 def test_resource_orders_all(): 
+    """
+        Test to get all orders
+    """
     response = client.get('api/v1/orders')
     assert(response.status_code == 200)
 
 def test_resource_order_add():
+    """
+        Test to add post with appropirate data
+    """
     response = client.post('api/v1/orders', data=order)
-    assert(response.status_code == 200)
-    
+    assert(response.status_code == 201)
+    assert 'chips' in str(response.data)
+
+def test_resource_order_add_without_data(): 
+    """
+        Test post without data
+    """
+    response = client.post('api/v1/orders', data=empty_order)
+    assert response.status_code == 400
+
+def test_resource_add_without_status():
+    """
+        Test post withot status
+    """
+    response = client.post('api/v1/orders', data=status_less_order)
+    assert response.status_code == 400
+
+def test_resource_add_without_price():
+    """
+        Test post withot price
+    """
+    response = client.post('api/v1/orders', data=price_less_order)
+    assert response.status_code == 400
+
+def test_resource_add_without_quantity():
+    """
+        Test post withot quantity
+    """
+    response = client.post('api/v1/orders', data=quantity_less_order)
+    assert response.status_code == 400
+
+def test_resource_add_without_food():
+    """
+        Test post withot food
+    """
+    response = client.post('api/v1/orders', data=food_less_order)
+    assert response.status_code == 400
+
 def test_resource_order_get_by_id(): 
+    """
+        Test to get order by its Id
+    """
     response = client.get('api/v1/orders/1')
     assert(response.status_code == 200)
 
-def test_resource_order_edit(): 
-    response = client.put('api/v1/orders/1', data ={
-        "id": 5,
-        "food": "chips",
-        "quantity": 2, 
-        "price": 200,
-        "status": "completed"
-    })
-    assert(response.status_code == 200)
-    
+def test_resource_order_get_by_id_str(): 
+    """
+        Test to get order but str for id
+    """
+    response = client.get('api/v1/orders/e')
+    assert(response.status_code == 404)
 
+def test_resource_order_get_by_nonexisting_id(): 
+    """
+        Test to get order with non-existent id
+    """
+    response = client.get('api/v1/orders/4')
+    assert 'null' in str(response.data)
         
-
-
-
-
