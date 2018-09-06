80b04b3a82f17bac6bb82abdf2d0139272971a35
kevinene91 <kevinene91@gmail.com>
2018-09-06 22:46:03 +0300

    [Chore #160326839] add tests

--- test_orders.py
+++ test_orders.py
@@ -16,23 +16,7 @@ def test_resource_orders_all():
     response = client.get('api/v1/orders')
     assert(response.status_code == 200)
 
-def test_resource_order_add():
-    response = client.post('api/v1/orders', data=order)
-    assert(response.status_code == 200)
-    
-def test_resource_order_get_by_id(): 
-    response = client.get('api/v1/orders/1')
-    assert(response.status_code == 200)
 
-def test_resource_order_edit(): 
-    response = client.put('api/v1/orders/1', data ={
-        "id": 5,
-        "food": "chips",
-        "quantity": 2, 
-        "price": 200,
-        "status": "completed"
-    })
-    assert(response.status_code == 200)
     
 
         
