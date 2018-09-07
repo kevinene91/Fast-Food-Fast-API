da856dd6175c922e1328074016f3ef47d9f3f91a
kevinene91 <kevinene91@gmail.com>
2018-09-07 16:42:26 +0300

    [Bug] fix conlict

--- app/resources/orders.py
+++ app/resources/orders.py
@@ -3,9 +3,18 @@ from flask_restful import Resource
 from app.models import orders
 
 class OrderListResource(Resource):
-	"""
-		return all the orders
-	"""
-	def get(self): 
+	def get(self):
+		"""
+			return all the orders
+		"""
 		return {'orders':orders}
 
+
+class OrderResource(Resource): 
+    """
+		get specific order via its id
+	"""
+    def get(self,id):
+        return {'order': next(filter(lambda x:x['id'] == id, orders), None)}
+
+    
--- test_orders.py
+++ test_orders.py
@@ -17,7 +17,30 @@ def test_resource_orders_all():
     assert(response.status_code == 200)
 
 
-    
+def test_resource_order_get_by_id(): 
+    response = client.get('api/v1/orders/1')
+    assert(response.status_code == 200)
+
+def test_resource_order_get_by_id_str(): 
+    """
+        Test to get order but str for id
+    """
+    response = client.get('api/v1/orders/e')
+    assert(response.status_code == 404)
+
+def test_resource_order_get_by_nonexisting_id(): 
+    """
+        Test to get order with non-existent id
+    """
+    response = client.get('api/v1/orders/4')
+    assert 'null' in str(response.data)
+
+def test_resource_order_get_by_negative_id(): 
+    """
+        Test to get order with negative id
+    """
+    response = client.get('api/v1/orders/-4')
+    assert response.status_code == 404
 
         
 
