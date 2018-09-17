28e8c8324bffd6899eba996a6f91cb8424acf5a2
kevinene91 <kevinene91@gmail.com>
2018-09-17 09:55:30 +0300

    [Chore restructure app #160539670]

---
+++ app/api/v1/resources/orders.py
@@ -0,0 +1,83 @@
+
+from flask_restful import Resource, reqparse
+from ..models.order_model import order_obj
+from flask import jsonify
+
+class OrderListResource(Resource):
+
+	def get(self): 
+
+		"""
+		Return all the orders 
+		"""
+		allorders = order_obj.get_orders()
+		if allorders:
+			return allorders
+		else:
+			return {"mesage":"no orders present"}
+		
+
+	def post(self):
+		"""
+		Post an order fields input, food, quantity, price, status
+		"""
+		inc_id = order_obj.get_length() + 1
+		parser = reqparse.RequestParser()
+		parser.add_argument("food",type=str,
+		required=True)
+
+		parser.add_argument("quantity",type=int,
+		required=True)
+		data = parser.parse_args()
+		
+		#check if food is in menu
+		food_item = order_obj.get_food_by_name(data['food'],order_obj.get_foods())
+		if food_item:
+			food_price = food_item['price']   
+			total = order_obj.calculate_total_price(food_price,data['quantity'])
+			#add default order items
+			order = {
+			'id': inc_id, "customer_name":"nesh",'food':data['food'],
+			'quantity':data['quantity'],'total':total,
+			'status':"pending" 
+			}
+			order_obj.add_order(order)
+			return order, 201
+		else:
+			return {'message': 'no such food item in menu'}, 404
+
+
+class OrderResource(Resource): 
+	def get(self,id):
+		"""
+			get a specicfic order via its id
+		"""
+		#filter list elements that do no have the id 
+		order = order_obj.get_by_id(id,order_obj.get_orders())
+		if order:
+			return order, 201
+		else:
+			message =  "order {} does not exits".format(id)
+			return {"message":message}, 404
+
+
+	def put(self,id):
+		"""
+			get an order by its id and update it
+		"""
+		# get input
+		parser = reqparse.RequestParser()
+		parser.add_argument("status",
+				type=str,
+				required=True,
+		)
+		data = parser.parse_args()
+		order_to_edit = order_obj.get_by_id(id,order_obj.get_orders())
+
+		#update order if found
+		if order_to_edit:
+			order_to_edit.update(data)
+		else:
+			return {"message":"no item to update"}, 400
+		return order_to_edit, 201
+
---
+++ app/tests/v1/test_orders.py
@@ -0,0 +1,96 @@
+import pytest
+import json
+from ... import create_app
+
+app = create_app(config_name="testing")
+client = app.test_client()
+
+order = {
+
+    "food": "chips",
+    "quantity": 2, 
+}
+
+empty_order = {}
+
+quantity_less_order = {
+ 
+    "food": "chips", 
+   
+}
+
+food_less_order = {
+    "quantity": 3, 
+}
+
+        
+def test_resource_orders_all(): 
+    """
+        Test to get all orders
+    """
+    response = client.get('api/v1/orders')
+    assert(response.status_code == 200)
+
+def test_resource_order_add():
+    """
+        Test to add post with appropirate data
+    """
+    response = client.post('api/v1/orders', data=order)
+    assert(response.status_code == 201)
+    assert 'chips' in str(response.data)
+
+def test_resource_order_add_without_data(): 
+    """
+        Test post without data
+    """
+    response = client.post('api/v1/orders', data=empty_order)
+    assert response.status_code == 400
+
+def test_resource_add_without_quantity():
+    """
+        Test post withot quantity
+    """
+    response = client.post('api/v1/orders', data=quantity_less_order)
+    assert response.status_code == 400
+
+def test_resource_add_without_food():
+    """
+        Test post withot food
+    """
+    response = client.post('api/v1/orders', data=food_less_order)
+    assert response.status_code == 400
+
+def test_resource_order_get_by_id(): 
+    """
+        Test to get order by its Id
+    """
+    response = client.get('api/v1/orders/1')
+    assert(response.status_code == 200)
+
+def test_resource_order_get_by_id_str(): 
+    """
+        Test to get order but str for id
+    """
+    response = client.get('api/v1/orders/e')
+    assert(response.status_code == 404)
+
+def test_resource_order_get_by_nonexisting_id(): 
+    """
+        Test to get order with non-existent id
+    """
+    response = client.get('api/v1/orders/4')
+    assert 'null' in str(response.data)
+
+def test_resource_order_edit(): 
+    """
+        Test edit by geting right id
+    """
+    response = client.put('api/v1/orders/1', data ={
+        "id": 5,
+        "food": "chips",
+        "quantity": 2, 
+        "price": 200,
+        "status": "completed"
+    })
+    assert(response.status_code == 201)
+    assert 'completed' in str(response.data)
