521624e5ddc9f283fa83fc21d07557c1744154b2
kevinene91 <kevinene91@gmail.com>
2018-10-03 11:01:46 +0300

    [Feature #160783015] PR review changes

--- instance/config.py
+++ instance/config.py
@@ -1,39 +1,42 @@
 
 import os 
-class Config(object): 
 
+
+class Config(object): 
     """
     Base config
-    """
-	DEBUG = False 
-	CSRF_ENABLED = True 
-	SECRET_KEY = os.getenv('SECRET_KEY')
-	POSTGRES_DATABASE_URI = os.getenv('DATABASE_URL')
+     """
+    DEBUG = False 
+    CSRF_ENABLED = True 
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    POSTGRES_DATABASE_URI = os.getenv('DATABASE_URL')
+
 
 class DevelopmentConfig(Config):
-	"""
-		Config for development
-	"""
-	DEBUG = True
+    """
+    Config for development
+    """
+    DEBUG = True
+
 
 class TestingConfig(Config): 
-	"""
-		Testing config
-	"""
-	Testing = True
-	DEBUG = True
-	POSTGRES_DATABASE_URI = os.getenv('TEST_DATABASE_URL')
+    """
+   Testing config
+    """
+    Testing = True
+    DEBUG = True
+    POSTGRES_DATABASE_URI = os.getenv('TEST_DATABASE_URL')
 
 
 class ProductionConfig(Config): 
-	"""
-		Production config
-	"""
-	DEBUG = False 
-	Testing = False
-
-app_config = {
-	'development': DevelopmentConfig, 
-	'production': ProductionConfig, 
-	'testing': TestingConfig,
-}
+    """
+    Production config
+    """
+    DEBUG = False 
+    Testing = False
+    
+    app_config = {
+        'development': DevelopmentConfig, 
+        'production': ProductionConfig, 
+        'testing': TestingConfig,
+        }
