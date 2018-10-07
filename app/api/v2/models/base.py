c0c8e9e57b2a2b58a8783c9713cdcf259417f128
kevinene91 <kevinene91@gmail.com>
2018-10-07 22:14:00 +0300

    [chore #161034240]

---
+++ app/api/v2/models/base.py
@@ -0,0 +1,56 @@
+from ..db.conn import create_conn
+from psycopg2.extras import RealDictCursor
+import psycopg2
+
+
+class BaseModel:
+    def __init__(self, data={}):
+        self.table = data.get('table_name')
+        self.db = create_conn()
+        self.id = data.get('id')
+        self.row = data.get('row')
+
+    def get_by_id(self):
+        con, response = self.db, None
+        cur = con.cursor(cursor_factory=RealDictCursor)
+        try:
+            cur.execute("""select * from '{}' WHERE '{}'='{}'
+                """.format(self.table, self.id, self.row))
+            response = cur.fetchone()
+        except psycopg2.DatabaseError as e:
+            return {'message': '{}'.format(e)}
+        con.close()
+        return response
+
+    def get_all(self):
+        con, response = self.db, None
+        cur = con.cursor(cursor_factory=RealDictCursor)
+        try:
+            cur.execute("select * from {}".format(self.table))
+            response = cur.fetchall()
+        except psycopg2.DatabaseError as e:
+            return {'message': '{}'.format(e)}
+        con.close()
+        return response
+
+    def delete(self):
+        con = self.db
+        cur = con.cursor(cursor_factory=RealDictCursor)
+        try:
+            cur.execute("""delete from '{}' where '{}' ='{}'
+            """.format(self.table, self.id, self.role))
+            con.commit()
+        except psycopg2.DatabaseError as e:
+            return {'message': '{}'.format(e)}
+        con.close()
+
+    def update(self):
+        con, response = self.db, None
+        cur = con.cursor(cursor_factory=RealDictCursor)
+        try:
+            cur.execute("""UPDATE '{}' SET '{}'='{}', {}='{}' WHERE '{}'='{}'
+            """.format(self.table, self.row, self.row, self.row, self.id))
+            con.commit()
+        except psycopg2.DatabaseError as e:
+            return {'message': '{}'.format(e)}
+        con.close()
--- app/api/v2/models/meals.py
+++ app/api/v2/models/meals.py
@@ -9,25 +9,12 @@ class MealModel:
         self.meal_id = data.get('meal_id')
         self.price = data.get('price')
         self.table = data.get('table_name')
-        self.db = create_conn()
-      
-    def get_by_id(self):
-        con, response = self.db, None
-        cur = con.cursor(cursor_factory=RealDictCursor)
-        try:
-            cur.execute("""select * from meals WHERE meal_id='{}'
-                """.format(self.meal_id))
-            response = cur.fetchone()
-        except psycopg2.DatabaseError as e:
-            return {'message': '{}'.format(e)}
-        con.close()
-        return response
+        self.db = create_conn()      
 
     def get_by_name(self):
         con, response = self.db, None
         cur = con.cursor()
         try:
-            
             cur.execute("""select * from meals WHERE meal_name='{}'
             """.format(self.meal_name))
             response = cur.fetchall()
@@ -36,50 +23,6 @@ class MealModel:
         con.close()
         return response
 
-    def update_name(self):
-        con, response = self.db, None
-        cur = con.cursor(cursor_factory=RealDictCursor)
-        try:
-            cur.execute("""UPDATE meals SET meal_name='{}', price='{}' WHERE meal_id='{}'
-            """.format(self.meal_name, self.price, self.meal_id))
-            con.commit()
-        except psycopg2.DatabaseError as e:
-            return {'message': '{}'.format(e)}
-        con.close()
+  
 
-    def get_all(self):
-        con, response = self.db, None
-        cur = con.cursor(cursor_factory=RealDictCursor)
-        try:
-            cur.execute("select * from {}".format(self.table))
-            response = cur.fetchall()
-        except psycopg2.DatabaseError as e:
-            return {'message': '{}'.format(e)}
-        con.close()
-        return response
-
-    def delete(self):
-        con = self.db
-        cur = con.cursor(cursor_factory=RealDictCursor)
-        try:
-            cur.execute("""delete from meals where meal_id='{}'
-            """.format(self.meal_id))
-            con.commit()
-        except psycopg2.DatabaseError as e:
-            return {'message': '{}'.format(e)}
-        con.close()
-
-    def save(self):
-        data, conn = None, self.db
-        cursor = conn.cursor(cursor_factory=RealDictCursor)
-        try:
-            query = """insert into meals (meal_name, price) values('{}','{}')
-            """.format(self.meal_name, self.price)
-            cursor.execute(query)
-            conn.commit()
-            data = cursor.fetchone()
-            cursor.close()   
-        except psycopg2.DatabaseError as e:
-            return {'message': '{}'.format(e)}
-        return data
             
